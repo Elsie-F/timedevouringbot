cdf0a8c41265af1c06ff5fbb10cb3df1d4df86f0
Elsie-F <e-filipp@yandex.ru>
2021-04-14 16:09:09 +0300

    use environment variables

--- src/main/java/com/elsief/telegram/TimeDevouringApp.java
+++ src/main/java/com/elsief/telegram/TimeDevouringApp.java
@@ -5,19 +5,20 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import static java.lang.System.getenv;
+
 public class TimeDevouringApp {
     private static final Logger log = Logger.getLogger(TimeDevouringApp.class);
-    private static final String BOT_NAME = "TestTimeDevouringBot";
-    private static final String BOT_TOKEN = "1764838700:AAGAEUH2GpgN2iitAXUodVTA3c9lJ3Vst-Y";
-    private static final int INTERVAL_IN_SECONDS = 5;
 
     public static void main(String[] args) {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             try {
-                botsApi.registerBot(new Bot(BOT_NAME, BOT_TOKEN, INTERVAL_IN_SECONDS));
-            } catch (TelegramApiException e) {
-                log.error("Error while registering bot", e);
+                botsApi.registerBot(new Bot(getenv("BOT_NAME"), getenv("BOT_TOKEN"), Integer.parseInt(getenv("INTERVAL_IN_SECONDS"))));
+            } catch (NumberFormatException numEx) {
+                log.error("Interval in seconds should be a number!", numEx);
+            } catch (TelegramApiException telEx) {
+                log.error("Error while registering bot", telEx);
             }
         } catch (Exception e) {
             log.error("Error while creating bots API", e);
